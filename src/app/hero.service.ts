d4e1468fc79febf5673904619854dc738f215b42
George Matos <georgemattos@gmail.com>
2020-11-22 15:09:29 -0300

    Add Messages

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { Hero } from './hero';
+import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
 
 @Injectable({
@@ -9,9 +10,10 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes')
     // simula um atraso de 0,5s
     return of(HEROES).pipe(delay(500));
   }
